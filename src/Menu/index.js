e78c17852257d9ab36be1658d3b197025fbe1278
EC2 Default User <ec2-user@ip-172-31-7-18.us-east-2.compute.internal>
2018-08-23 08:39:42 +0000

    initial commit

---
+++ src/Menu/index.js
@@ -0,0 +1,96 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import classnames from 'classnames'
+import './menu.css'
+import ShapeOverlays from './ShapeOverlays'
+
+const withMenu = props => WrappedComponent => {
+  return class extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        isMenuVisible: false,
+      }
+      this.overlay = null
+      this.toggleMenu = this.toggleMenu.bind(this);
+      this.handleNavItemClick = this.handleNavItemClick.bind(this);
+    }
+
+    toggleMenu(event){
+      event && event.preventDefault()
+      if (!this.overlay || this.overlay.isAnimating) {
+        return false
+      }
+      this.overlay.toggle()
+      this.setState({ isMenuVisible: !this.state.isMenuVisible })
+    }
+
+    handleNavItemClick (item) {
+      return event => {
+        this.toggleMenu(event)
+        item.action(this.props)
+      }
+    }
+
+    componentDidMount() {
+      const node = ReactDOM.findDOMNode(this)
+      this.overlay = new ShapeOverlays(node.querySelector('.shape-overlays'))
+    }
+
+    render() {
+      const { items } = props
+      const { isMenuVisible } = this.state
+
+      return (
+        <div className="new-menu">
+          <div className="content--new-menu">
+            <div
+              className={classnames('hamburger hamburger--new-menu js-hover', { 'is-opened-navi': isMenuVisible })}
+              onClick={this.toggleMenu}
+            >
+              <div className="hamburger__line hamburger__line--01">
+                <div className="hamburger__line-in hamburger__line-in--01" />
+              </div>
+              <div className="hamburger__line hamburger__line--02">
+                <div className="hamburger__line-in hamburger__line-in--02" />
+              </div>
+              <div className="hamburger__line hamburger__line--03">
+                <div className="hamburger__line-in hamburger__line-in--03" />
+              </div>
+              <div className="hamburger__line hamburger__line--cross01">
+                <div className="hamburger__line-in hamburger__line-in--cross01" />
+              </div>
+              <div className="hamburger__line hamburger__line--cross02">
+                <div className="hamburger__line-in hamburger__line-in--cross02" />
+              </div>
+            </div>
+            <WrappedComponent {...this.props} toggleMenu={this.toggleMenu} />
+            <div className="global-menu">
+              <div className="global-menu__wrap">
+                {items.map((item, i) => (
+                  <a
+                    key={i}
+                    className={classnames('global-menu__item global-menu__item--new-menu', {
+                      'is-opened': isMenuVisible,
+                    })}
+                    href="#"
+                    onClick={this.handleNavItemClick(item)}
+                  >
+                    {item.label}
+                  </a>
+                ))}
+              </div>
+            </div>
+            <svg ref="test" className="shape-overlays" viewBox="0 0 100 100" preserveAspectRatio="none">
+              <path className="shape-overlays__path" />
+              <path className="shape-overlays__path" />
+              <path className="shape-overlays__path" />
+            </svg>
+          </div>
+        </div>
+      )
+    }
+  }
+}
+
+export default withMenu
